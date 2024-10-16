992f856a81c35094fbe028509c865d7a129cebb2
Muba2520 <mubamistry2520@gmail.com>
2024-10-16 17:49:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # This is a test file of the OTA code
-
+from machine import Pin
+import utime
 print('Hello World')
 print("Hello")
 print("this is another line test, written on an iPad using VS Code for web.")
+
+led = Pin(2,Pin.OUT)
+
+while True:
+  led.value(1)
+  ultime.sleep(1)
+  led.value(0)
+  ultime.sleep(1)
