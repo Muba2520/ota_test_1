df1055f638a79a446bac3eeb8a0bdc64c9386f68
Muba2520 <mubamistry2520@gmail.com>
2024-10-17 15:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,10 +28,7 @@ timer.init(period=10000, mode=machine.Timer.PERIODIC, callback=check_update)
 
 
 while True: 
-   
-      
-   for i in range(10):
       led.value(1)
-      utime.sleep(2)
+      utime.sleep(0.1)
       led.value(0)
-      utime.sleep(2)
+      utime.sleep(0.1)
