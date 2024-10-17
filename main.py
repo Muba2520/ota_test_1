106da733bec576694d1608dfcf61a1a4ecb6de38
Muba2520 <mubamistry2520@gmail.com>
2024-10-17 14:28:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ print("Hello")
 print("this is another line test, written on an iPad using VS Code for web.")
 
 led = Pin(2,Pin.OUT)
+time1 = utime.time()
 
 while True:
-   led.value(1)
-   utime.sleep(1)
-   led.value(0)
-   utime.sleep(1)
+   time2 = utime.time()
+   ftime= time2 - time1
+   if ftime >= 10:
+      ota_updater.download_and_install_update_if_available()
+   for i inrange(10):
+      led.value(1)
+      utime.sleep(1)
+      led.value(0)
+      utime.sleep(1)
