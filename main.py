7d707b4db2e8c026497206a1a1c2f5379e59846b
Muba2520 <mubamistry2520@gmail.com>
2024-10-17 10:04:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ print("this is another line test, written on an iPad using VS Code for web.")
 led = Pin(2,Pin.OUT)
 
 while True:
-# num = int(input("Etter a number : "))
-# for i in range(5):
    led.value(1)
    utime.sleep(0.1)
    led.value(0)
