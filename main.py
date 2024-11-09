71472091b014eff364e52d2cfa468977a19c222b
Muba2520 <mubamistry2520@gmail.com>
2024-11-09 11:12:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ led = machine.Pin('LED',machine.Pin.OUT)
 
 while True:
       led.value(1)
-      utime.sleep(0.1)
+      utime.sleep(2)
       led.value(0)
-      utime.sleep(0.1)
+      utime.sleep(2)
       
